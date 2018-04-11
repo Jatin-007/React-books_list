3b8b7f4d690cc821eea4cc3fb73ca900d504222c
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-10 19:10:41 -0700

    Implementing Actions and Reducers + Added Bootstrap

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default ()=> {
+    return [
+        {title: 'Introduction to Algorithms'},
+        {title: 'Cracking the Coding Interview'},
+        {title: 'Crushing It!, Gary Vaynerchuk'},
+        {title: 'Elon Musk'},
+    ]
+}
