ee73ee9503b2ce64857622acbb1a25981bb09531
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-11 00:16:38 -0700

    Integrating Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import BookList from './containers/book-list';
+
 class App extends Component {
   render() {
     return (
       <div>
-        Book List
+        <BookList />
       </div>
     );
   }
