c1b5b5b5e2a283d48069c03172f0ca59e67fce93
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-14 02:10:30 -0700

    Minor UI change

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -13,19 +13,19 @@ class BookDetail extends Component {
             
             <div>
                 <h3 className="book-title">{this.props.book.title}</h3>
-                <hr/>
+                <hr />
             </div>
 
-            <div>
-                <div>
+            <div className="row">
+                <div className="container">
                     By: {this.props.book.author}<br/>
                     {this.props.book.description}<br/>  
-                    <img className="img-thumbnail float-right col-md-4" src={this.props.book.image} alt="image"/>
+                    <img className="img-thumbnail float-right col-md-4 book-image" src={this.props.book.image} alt="image"/>
                 </div>
                 <div className="col-md-12">
                     <p>
                         <div className="card col-md-3 font-weight-bold">Price: {this.props.book.price}</div>
-                        <a href={this.props.book.url} className="float-right"> BUY NOW! </a>
+                        <button className="btn btn-light float-right"><a href={this.props.book.url}> BUY NOW! </a></button>
                     </p>
                 </div>
                 </div>
